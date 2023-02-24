a395cd00f6d6014d91e66bb45c512f8a0cd84c21
Carlos Gómez Fernández <carlos.gomez33@alu.uclm.es>
2023-02-24 11:11:25 +0100

    routes app

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent implements OnInit {
 
+export class MenuComponent implements OnInit {
   items: MenuItem[] = [];
 
   constructor() { }
